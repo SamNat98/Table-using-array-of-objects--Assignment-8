0bbf934e4ecf69248f63d3b6adc546c8917a3a8e
sameer khanna <sameerkhanna1998@gmail.com>
2021-07-27 12:04:34 +0530

    Table using array of objects

---
+++ 1.js
@@ -0,0 +1,211 @@
+var arr = [
+    {
+        "emp_name": "Rohan",
+        "emp_id": 1,
+        "emp_desg": "Software Developer",
+        "emp_explvl": 4
+    },
+    {
+        "emp_name": "Vicky",
+        "emp_id": 2,
+        "emp_desg": "Software Engineer",
+        "emp_explvl": 2
+    },
+    {
+        "emp_name": "John",
+        "emp_id": 3,
+        "emp_desg": "Graphic Designer",
+        "emp_explvl": 7
+    },
+    {
+        "emp_name": "Mike",
+        "emp_id": 4,
+        "emp_desg": "Sales Head",
+        "emp_explvl": 5
+    },
+    {
+        "emp_name": "Sahil",
+        "emp_id": 5,
+        "emp_desg": "Project Manager",
+        "emp_explvl": 10
+    },
+    {
+        "emp_name": "Ronald",
+        "emp_id": 6,
+        "emp_desg": "Software Tester",
+        "emp_explvl": 3.5
+    }
+];
+
+function rec() {
+
+    var id1 = document.getElementsByClassName("1");
+    var id2 = document.getElementsByClassName("2");
+    var id3 = document.getElementsByClassName("3");
+    var id4 = document.getElementsByClassName("4");
+    var id5 = document.getElementsByClassName("5");
+    var id6 = document.getElementsByClassName("6");
+
+    var a1 = Object.values(arr[0]);
+    var a2 = Object.values(arr[1]);
+    var a3 = Object.values(arr[2]);
+    var a4 = Object.values(arr[3]);
+    var a5 = Object.values(arr[4]);
+    var a6 = Object.values(arr[5]);
+
+
+    for (i = 0; i < a1.length; i++) {
+
+        id1[i].innerHTML = a1[i];
+    }
+
+    for (i = 0; i < a2.length; i++) {
+
+        id2[i].innerHTML = a2[i];
+    }
+
+    for (i = 0; i < a3.length; i++) {
+
+        id3[i].innerHTML = a3[i];
+    }
+
+    for (i = 0; i < a4.length; i++) {
+
+        id4[i].innerHTML = a4[i];
+    }
+    for (i = 0; i < a5.length; i++) {
+
+        id5[i].innerHTML = a5[i];
+    }
+
+    for (i = 0; i < a6.length; i++) {
+
+        id6[i].innerHTML = a6[i];
+    }
+
+}
+
+var i1, i2, i3, i4, bn, i5, i6, i7, i8, i9, cell1, cell2, cell3, cell4;
+
+function add() {
+
+    let z = document.getElementById("addform");
+
+    z.style.display = "block";
+
+    i1 = document.getElementById("ip1");
+    i2 = document.getElementById("ip2");
+    i3 = document.getElementById("ip3");
+    i4 = document.getElementById("ip4");
+    bn = document.getElementById("b5");
+
+    i5 = document.getElementById("h1");
+    i6 = document.getElementById("l1");
+    i7 = document.getElementById("l2");
+    i8 = document.getElementById("l3");
+    i9 = document.getElementById("l4");
+
+    let a = document.getElementById("tb");
+
+    let row = a.insertRow();
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+    cell3 = row.insertCell(2);
+    cell4 = row.insertCell(3);
+}
+
+function final() {
+    cell1.innerHTML = i1.value;
+    cell2.innerHTML = i2.value;
+    cell3.innerHTML = i3.value;
+    cell4.innerHTML = i4.value;
+
+    let z = document.getElementById("addform");
+
+    z.style.display = "none";
+
+
+}
+
+
+function remove() {
+
+
+    if (confirm("Are you sure,you want to delete the Record")) {
+
+        var gg = prompt("Enter the row number you want to remove");
+
+        let table = document.getElementById("tb");
+
+        let row = table.rows;
+        row[gg].remove();
+
+        let z = document.getElementById("addform");
+
+
+
+        if (z.style.display === "block") {
+            z.style.display = "none";
+        }
+
+
+    }
+
+
+}
+
+var y1, y2, y3, y4, y5, y6, y7, y8, y9, y10, input;
+
+function edit() {
+
+    let z = document.getElementById("editform");
+
+    z.style.display = "block";
+
+    y1 = document.getElementById("a");
+    y2 = document.getElementById("b");
+    y3 = document.getElementById("g1");
+    y4 = document.getElementById("c");
+    y5 = document.getElementById("c1");
+    y6 = document.getElementById("d");
+    y7 = document.getElementById("d1");
+    y8 = document.getElementById("e");
+    y9 = document.getElementById("e1");
+    y10 = document.getElementById("f1");
+
+    input = prompt("Which row you want to edit");
+
+    var tdb = document.getElementsByClassName(input);
+
+
+    console.log(tdb[0].innerHTML);
+    console.log(tdb[1].innerHTML);
+    console.log(tdb[2].innerHTML);
+    console.log(tdb[3].innerHTML);
+
+    y3.value = tdb[0].innerHTML;
+    y5.value = tdb[1].innerHTML;
+    y7.value = tdb[2].innerHTML;
+    y9.value = tdb[3].innerHTML;
+
+    console.log(y3.value);
+
+}
+
+function finall() {
+
+    var tdb = document.getElementsByClassName(input);
+
+    console.log(tdb[1].innerHTML);
+
+    tdb[0].innerHTML = y3.value;
+    tdb[1].innerHTML = y5.value;
+    tdb[2].innerHTML = y7.value;
+    tdb[3].innerHTML = y9.value;
+
+    let z = document.getElementById("editform");
+
+    z.style.display = "none";
+}
+
+rec();
